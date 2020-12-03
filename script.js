5fa8b9bd0205dac73d76cb3cc9f91884d056e02d
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2020-12-03 01:34:18 -0600

    added planetary data from fetched JSON document

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         let div = document.getElementById("missionTarget");
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[0].name}</li>
+            <li>Diameter: ${json[0].diameter}</li>
+            <li>Star: ${json[0].star}</li>
+            <li>Distance from Earth: ${json[0].distance}</li>
+            <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src="${json[0].image}">`;
+      });
+   });
+
    let form = document.getElementById("launchForm");
    form.addEventListener("submit", function(event){
       let pilotNameInput = document.querySelector("input[name=pilotName]");
