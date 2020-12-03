6ff2eea71b6745dd62c803e209e5e64c9423304a
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2020-12-03 01:18:07 -0600

    script updates shuttle requirements based on form input values

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ window.addEventListener("load", function(){
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
 
+      pilotStatus = document.getElementById("pilotStatus");
+      pilotStatus.innerHTML=`Pilot ${pilotNameInput.value} is ready for launch.`;
+      copilotStatus = document.getElementById("copilotStatus");
+      copilotStatus.innerHTML=`Co-pilot ${copilotName.value} is ready for launch.`;
+
+      faultyItems=document.getElementById("faultyItems");
+      cargoStatus=document.getElementById("cargoStatus");
+      fuelStatus = document.getElementById("fuelStatus");
+      launchStatus = document.getElementById("launchStatus");
+
+
       if (pilotNameInput.value.trim() === "" || copilotName.value.trim() === "" || fuelLevel.value.trim() === "" || cargoMass.value.trim() === ""){
          alert("All fields are required!");
          event.preventDefault();
@@ -14,12 +25,34 @@ window.addEventListener("load", function(){
       } else if (!isNaN(pilotNameInput.value) || !isNaN(copilotName.value)){
          alert("Make sure to enter valid information for each field!");
          event.preventDefault();
-         
+
       } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
          alert("Make sure to enter valid information for each field!");
          event.preventDefault();
-      }
-   });
+
+      } else if (Number(fuelLevel.value) < 10000){
+         fuelStatus.innerHTML="Fuel level too low for launch.";
+         faultyItems.style.visibility="visible";
+         launchStatus.innerHTML="Shuttle not ready for launch";
+         launchStatus.style.color="red";
+
+         if (Number(cargoMass.value) > 10000){
+            cargoStatus.innerHTML="Cargo mass too high for launch";
+         }
+
+          } else if (Number(cargoMass.value) > 10000){
+            cargoStatus.innerHTML="Cargo mass too high for launch";
+            faultyItems.style.visibility="visible";
+            launchStatus.innerHTML="Shuttle not ready for launch";
+            launchStatus.style.color="red";
+
+          } else {
+            faultyItems.style.visibility="visible";
+             launchStatus.innerHTML="Shuttle is ready for launch";
+             launchStatus.style.color="green";
+          }
+      event.preventDefault();
+   });    
 });
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
